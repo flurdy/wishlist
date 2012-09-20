4b7477e0316baec307eefd19419cbaa57dbf6c79
ivar.abrahamsen <ivar@flurdy.com>
2012-09-21 00:24:53 +0100

    show vwish. log in / register link

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -6,12 +6,15 @@
 insert into recipient (recipientid,username,fullname,email,password) 
 	values ((select NEXTVAL('recipient_seq')),'testuser','Test user','test@example.com','$2a$10$.LMFHFigeUeZwg3VZgZr3ekcKF6xFjjcnfPPxlTwJ0MAsdPGxXf8y');
 
+insert into recipient (recipientid,username,fullname,email,password) 
+	values ((select NEXTVAL('recipient_seq')),'anotheruser','Another user','test@example.com','$2a$10$.LMFHFigeUeZwg3VZgZr3ekcKF6xFjjcnfPPxlTwJ0MAsdPGxXf8y');
+
 
 insert into wishlist (wishlistid,title,description,recipientid)
 	values ((select NEXTVAL('wishlist_seq')),'Christmas list for Ivar','',(select recipientid from recipient where username='testuser'));
 
 insert into wishlist (wishlistid,title,description,recipientid)
-	values ((select NEXTVAL('wishlist_seq')),'Main list for Xerxes','',(select recipientid from recipient where username='testuser'));
+	values ((select NEXTVAL('wishlist_seq')),'Main list for Xerxes','',(select recipientid from recipient where username='anotheruser'));
 
 insert into wish (wishid,title,description,wishlistid)
 	values ((select NEXTVAL('wish_seq')),'Red car','',(select wishlistid from wishlist where title='Christmas list for Ivar'));
