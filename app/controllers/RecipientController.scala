aa049718483acb7c7f110f969b2a7a10abee7d33
ivar <ivar@flurdy.com>
2012-09-22 00:08:32 +0100

    Diffing new schortcut link to spanish+esp

--- app/controllers/RecipientController.scala
+++ app/controllers/RecipientController.scala
@@ -8,16 +8,40 @@ import models._
 
 
 object RecipientController extends Controller with Secured {
-	
-    def findWishlistsByRecipient(username:String) = Action { implicit request =>
-        val wishlists = Wishlist.findWishlistsByUsername(username)
-        Ok(views.html.recipient.profile(wishlists))
-   }
 
 
-	def showRecipient(username:String) = Action {
+	def showProfile(username:String) = Action {  implicit request =>
+    val recipient = Recipient.findByUsername(username).get
+    val wishlists = Wishlist.findWishlistsByUsername(username)
+    Ok(views.html.recipient.profile(recipient,wishlists))
+  }
 
-	}	
+  def showEditRecipient(username:String) = withCurrentRecipient { currentRecipient => implicit request =>
+    val recipient = Recipient.findByUsername(username).get
+    Ok(views.html.recipient.editrecipient(recipient))
+  }
+
+  def showDeleteRecipient(username:String) = withCurrentRecipient { currentRecipient => implicit request =>
+    val recipient = Recipient.findByUsername(username).get
+    Ok(views.html.recipient.deleterecipient(recipient))
+  }
+
+
+  def deleteRecipient(username:String) = withCurrentRecipient { currentRecipient => implicit request =>
+    val recipient = Recipient.findByUsername(username).get
+    recipient.delete
+    Redirect(routes.Application.index()).flashing("messageWarning" -> "Recipient deleted")
+  }
+
+
+  def updateRecipient(username:String)  = withCurrentRecipient { currentRecipient => implicit request =>
+    val recipient = Recipient.findByUsername(username).get
+
+    Redirect(routes.RecipientController.showEditRecipient(username)).flashing("messageWarning" -> "Recipient updated")
+  }
+
+
+
 
 }
 
