2b13081f24fd6b43bfafce732e6ff6ad558f9211
ivar <ivar@flurdy.com>
2012-11-01 00:01:52 +0000

    wish entry table and migrate data

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -3,7 +3,18 @@
 # --- !Ups
 
 
+CREATE TABLE wishentry (
+	wishid		BIGINT REFERENCES wish (wishid) ON DELETE CASCADE,
+	wishlistid		BIGINT REFERENCES wishlist (wishlistid) ON DELETE CASCADE,
+	ordinal		INT
+);
+
+INSERT INTO wishentry (wishid,wishlistid,ordinal)
+	SELECT wishid,wishlistid,ordinal FROM wish;
 
 
 # --- !Downs
+
+ 
+DROP TABLE IF EXISTS wishentry;
 
